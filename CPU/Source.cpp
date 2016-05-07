df20ffaf6a8f13197e0d36833002c67ce749cf3c
claud333 <333claud@gmail.com>
2016-05-07 22:21:50 +0300

    mamory

--- CPU/Source.cpp
+++ CPU/Source.cpp
@@ -16,27 +16,16 @@ int main()
 	cout << "Заполнение памяти по умолчанию" << endl;
 	theMemory->Write(1, 1);
 	theMemory->Write(2, 1);
-
-	// Load1 Value 2
 	theMemory->Write(3, 2);
 	theMemory->Write(4, 2);
-
-	// Add
 	theMemory->Write(5, 3);
-
-	// Store to 12
 	theMemory->Write(6, 5);
 	theMemory->Write(7, 12);
-
-	// Print from 12
 	theMemory->Write(8, 6);
 	theMemory->Write(9, 12);
-
-	// Beep
 	theMemory->Write(10, 4);
-
-	// HALT
 	theMemory->Write(11, 0);
+
 	cout << "Показать значение ячеек памяти?\nY/N\n";
 	char yesNo;
 	cin >> yesNo;
