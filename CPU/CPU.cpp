7365bcc378ea5fde0a29c646b9cd34ee4b631214
ilia922 <ilia922@mail.ru>
2016-05-08 09:54:25 +0300

    memory

--- CPU/CPU.cpp
+++ CPU/CPU.cpp
@@ -1,6 +1,32 @@
 #include "CPU.h"
 namespace CPU_ilia
 {
+	CPU::CPU(Memory* takeMemory)
+		:
+		CPU_theMemory(takeMemory),
+		startAddres(1),
+		endAddres(253),
+		programCounter(startAddres),
+		F_Halt(false),
+		F_error(false)
+	{
+	}
+
+	CPU::~CPU()
+	{
+		CPU_theMemory = nullptr;
+	}
+		
+
+	void CPU::Halt()
+	{
+		F_Halt = true;
+	}
+
+	void CPU::error()
+	{
+		F_error = true;
+	}
 
 
 
--- CPU/CPU.h
+++ CPU/CPU.h
@@ -8,14 +8,18 @@ namespace CPU_ilia
 	public:
 		const byte startAddres; const byte endAddres;
 	private:
-		byte Register_0; byte Registr_1; 
-		byte saveAddres;
+		byte Register_0; byte Registr_1; byte Registr_2;
 		byte programCounter;
 		Memory* CPU_theMemory;
+		bool F_Halt; bool F_error;
 	public:
-		CPU();
+		CPU(Memory* takeMemory);
 		~CPU();
-		const byte Featch();
+		const byte Featch(const byte &addres);
+		void Print(const byte &Register_addres, const byte &Registor_valou);
+		void Add();
+		void Halt();
+		void error();
 		void Decod(const byte &valou);
 		
 
